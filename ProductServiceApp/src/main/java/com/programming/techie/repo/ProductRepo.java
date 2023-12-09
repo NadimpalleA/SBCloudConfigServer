2b9efd859c8f193724ed0df2e8ba4405337344c9
NadimpalleA <anuradha.na16@gmail.com>
2023-12-09 11:58:16 +0530

    first commit

--- ProductServiceApp/src/main/java/com/programming/techie/repo/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.programming.techie.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.programming.techie.entity.Product;
-
-public interface ProductRepo extends MongoRepository<Product, Integer> {
-
-}
