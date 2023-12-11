aaf1fe602a62dd92580f7f63bbddfb7ad544d7a2
NadimpalleA <anuradha.na16@gmail.com>
2023-12-11 11:06:24 +0530

    config server

---
+++ ConfigServerApp/src/main/java/com/programming/techie/ConfigServerAppApplication.java
@@ -0,0 +1,16 @@
+package com.programming.techie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerAppApplication.class, args);
+		System.out.println("Config Server App");
+	}
+
+}
